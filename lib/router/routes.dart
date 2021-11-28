7839f5c53c78b6ad2ff4cfbc704deaeb2c17eb16
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-11-28 10:47:10 +0600

    tasks-page routes

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/app/groups/groups_page.dart';
 import 'package:to_do_app/app/groups/group_form_page.dart';
+import 'package:to_do_app/app/tasks/task_form_page.dart';
+import 'package:to_do_app/app/tasks/tasks_page.dart';
 import 'package:to_do_app/router/not_found_router_page.dart';
 
 class RoutesData {
   static final routes = {
     '/groups': (context) => const GroupsPage(),
     '/groups/form': (context) => const GroupFormPage(),
+    '/groups/tasks': (context) => const TasksPage(),
+    '/groups/tasks/form': (context) => const TasksFormPage(),
   };
 
   static const initialRoute = '/groups';
