fe196d0c9a1f45da289dd682f27c8a821c62b34a
Beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-08 18:41:34 +0600

    new file stucture

--- lib/app/tasks/tasks_page.dart
+++ lib/app/tasks/tasks_page.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_app/app/groups/models/groups_model.dart';
-import 'package:to_do_app/app/tasks/widgets/task_list_item_widget.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({Key? key}) : super(key: key);
@@ -10,52 +8,8 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
-  final _model = GroupsModel();
-
   @override
   Widget build(BuildContext context) {
-    return GroupsModelProvider(
-      model: _model,
-      child: const _GroupsPageBody(),
-    );
-  }
-}
-
-class _GroupsPageBody extends StatelessWidget {
-  const _GroupsPageBody({Key? key}) : super(key: key);
-
-  void handleShowForm(BuildContext context) {
-    Navigator.pushNamed(context, '/groups/tasks/form');
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final model = GroupsModelProvider.of(context)!.model;
-    final groupsCount = model.groups.length;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tasks'),
-      ),
-      body: ListView.separated(
-        itemCount: groupsCount,
-        itemBuilder: (BuildContext context, int index) {
-          final group = model.groups[index];
-          return TaskListItemWidget(
-            title: group.name,
-            index: index,
-            handleRemove: () {},
-            handleTap: () {},
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) {
-          return const Divider(height: 2);
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => handleShowForm(context),
-        child: const Icon(Icons.add),
-      ),
-    );
+    return Container();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:to_do_app/hive/hive_init.dart';
 import 'app/app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  initHiveAdapters();
+  await initHiveBoxes();
+  
   runApp(const App());
 }
