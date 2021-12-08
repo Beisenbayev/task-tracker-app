d78a5e021e12a360587fe82482af321302d811f1
Beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-08 19:00:09 +0600

    tasks-page model activation

--- lib/app/tasks/tasks_page.dart
+++ lib/app/tasks/tasks_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/core/models/tasks_model.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({Key? key}) : super(key: key);
@@ -8,8 +9,30 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
+  final model = TasksModel();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return TasksModelProvider(
+      child: _TasksPageContent,
+      model: model,
+    );
+  }
+}
+
+class _TasksPageContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final model = TasksModelProvider.of(context)!.model;
+    final tasksCount = model.tasks.length;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Tasks')),
+      body: ListView.separated(
+        itemCount: tasksCount,
+        itemBuilder: (BuildContext context, int index) => Container(),
+        separatorBuilder: (BuildContext context, int index) => Container(),
+      ),
+    );
   }
 }
