8a173b600aed384d5d49d778289348f373e7c07c
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-09 14:34:10 +0600

    edit task page route

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/app/tasks/create_task_page.dart';
+import 'package:to_do_app/app/tasks/edit_task_page.dart';
 import 'package:to_do_app/app/tasks/tasks_page.dart';
 import 'package:to_do_app/router/not_found_router_page.dart';
 
 class RoutesData {
   static final routes = {
     '/tasks': (context) => const TasksPage(),
-    '/tasks/form': (context) => CreateTaskPage()
+    '/tasks/form': (context) => CreateTaskPage(),
+    '/tasks/edit-task': (context) => EditTaskPage(),
   };
 
   static const initialRoute = '/tasks';
