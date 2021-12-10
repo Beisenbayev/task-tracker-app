9c2c3c6bf6c0f053a3348a7e69938a371b95abd0
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-10 23:49:01 +0600

    archives page make up

--- lib/app/archives/archives_page.dart
+++ lib/app/archives/archives_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/app/archives/widgets/archive_list_item_widget.dart';
 import 'package:to_do_app/core/widgets/navigation_bar.dart';
 
 class ArchivesPage extends StatefulWidget {
@@ -11,12 +12,31 @@ class ArchivesPage extends StatefulWidget {
 class _ArchivesPageState extends State<ArchivesPage> {
   @override
   Widget build(BuildContext context) {
+    return _ArchivesPageContent();
+  }
+}
+
+class _ArchivesPageContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Archives'),
       ),
-      body: const Center(
-        child: Text('archives'),
+      body: ListView.separated(
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return ArchiveListItemWidget(
+            index: index,
+            title: 'title',
+            subtitle: 'subtitle',
+            handleRemove: () {},
+            handleUnarchive: () {},
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider(height: 2);
+        },
       ),
       bottomNavigationBar: NavigationBarWidget.content(context),
     );
