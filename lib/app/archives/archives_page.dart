6e12df9a737d760823532a0e8d60c51806c8007c
Beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-11 10:26:37 +0600

    archives page was provided with a model

--- lib/app/archives/archives_page.dart
+++ lib/app/archives/archives_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/app/archives/widgets/archive_list_item_widget.dart';
+import 'package:to_do_app/core/models/archives_model.dart';
 import 'package:to_do_app/core/widgets/navigation_bar.dart';
 
 class ArchivesPage extends StatefulWidget {
@@ -10,28 +11,38 @@ class ArchivesPage extends StatefulWidget {
 }
 
 class _ArchivesPageState extends State<ArchivesPage> {
+  final _model = ArchivesModel();
+
   @override
   Widget build(BuildContext context) {
-    return _ArchivesPageContent();
+    return ArchivesModelProvider(
+      child: _ArchivesPageContent(),
+      model: _model,
+    );
   }
 }
 
 class _ArchivesPageContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final model = ArchivesModelProvider.of(context)!.model;
+    final archivesCount = model.archives.length;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Archives'),
       ),
       body: ListView.separated(
-        itemCount: 5,
+        itemCount: archivesCount,
         itemBuilder: (BuildContext context, int index) {
+          final archive = model.archives[index];
+
           return ArchiveListItemWidget(
             index: index,
-            title: 'title',
-            subtitle: 'subtitle',
-            handleRemove: () {},
-            handleUnarchive: () {},
+            title: archive.title,
+            subtitle: archive.text,
+            handleRemove: model.removeTask,
+            handleUnarchive: model.unarchiveTask,
           );
         },
         separatorBuilder: (BuildContext context, int index) {
