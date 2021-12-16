e46e40123986f02c7383c3654c0a89cac0cc3bcd
beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-16 23:45:58 +0600

    color-data-module was changed

--- lib/app/tasks/modules/colors_data.dart
+++ lib/app/tasks/modules/colors_data.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
 
-class ColorData {
-  final int id;
-  final Color color;
-
-  ColorData({
-    required this.id,
-    required this.color,
-  });
-}
-
 class ColorsCollection {
-  static final List<ColorData> colors = [
-    ColorData(id: 1, color: Colors.grey),
-    ColorData(id: 2, color: const Color(0xFF4f81bc)),
-    ColorData(id: 3, color: const Color(0xFF8064a1)),
-    ColorData(id: 4, color: const Color(0xFFfea500)),
-    ColorData(id: 5, color: const Color(0xFFfe0000)),
-  ];
+  static final Map<int, Color> colors = {
+    0: Colors.grey,
+    1: const Color(0xFF4f81bc),
+    2: const Color(0xFF8064a1),
+    3: const Color(0xFFfea500),
+    4: const Color(0xFFfe0000),
+  };
 }
