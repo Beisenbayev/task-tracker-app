47074842920a41f6bc29cacd09f8a5d4e43f3aa9
Beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-17 16:09:30 +0600

    archive-list-item style

--- lib/app/archives/widgets/archive_list_item_widget.dart
+++ lib/app/archives/widgets/archive_list_item_widget.dart
@@ -19,38 +19,49 @@ class ArchiveListItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Slidable(
-      child: ListTile(
-        title: Text(title),
-        subtitle: Text(
-          subtitle,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
+    return Card(
+      elevation: 3,
+      clipBehavior: Clip.hardEdge,
+      child: Slidable(
+        child: Container(
+          decoration: const BoxDecoration(
+            border: Border(
+              left: BorderSide(color: Colors.grey, width: 10),
+            ),
+          ),
+          child: ListTile(
+            title: Text(title),
+            subtitle: Text(
+              subtitle,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
         ),
-      ),
-      startActionPane: ActionPane(
-        motion: const ScrollMotion(),
-        children: [
-          SlidableAction(
-            onPressed: (BuildContext context) => handleUnarchive(index),
-            backgroundColor: const Color(0xFF21B7CA),
-            foregroundColor: Colors.white,
-            icon: Icons.unarchive,
-            label: 'Unarchive',
-          ),
-        ],
-      ),
-      endActionPane: ActionPane(
-        motion: const ScrollMotion(),
-        children: [
-          SlidableAction(
-            onPressed: (BuildContext context) => handleRemove(index),
-            backgroundColor: const Color(0xFFFE4A49),
-            foregroundColor: Colors.white,
-            icon: Icons.delete,
-            label: 'Delete',
-          ),
-        ],
+        startActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (BuildContext context) => handleUnarchive(index),
+              backgroundColor: const Color(0xFF21B7CA),
+              foregroundColor: Colors.white,
+              icon: Icons.unarchive,
+              label: 'Unarchive',
+            ),
+          ],
+        ),
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (BuildContext context) => handleRemove(index),
+              backgroundColor: const Color(0xFFFE4A49),
+              foregroundColor: Colors.white,
+              icon: Icons.delete,
+              label: 'Delete',
+            ),
+          ],
+        ),
       ),
     );
   }
