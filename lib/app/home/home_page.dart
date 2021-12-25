ce8b1c5a0cbe5a51c5fb8ee2c98a2fbb76517878
Beisenbayev <beisenbayeff.nursultan@mail.ru>
2021-12-25 14:21:22 +0600

    home-page main screen

---
+++ lib/app/home/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_app/core/widgets/tap_bar_widget.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _index = 0;
+
+  void _changeIndex(int index) {
+    setState(() {
+      _index = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            TapBarWidget(
+              currentIndex: _index,
+              items: const <TapBarWidgetItem>[
+                TapBarWidgetItem(text: 'Task List'),
+                TapBarWidgetItem(text: 'Completed')
+              ],
+              onTap: _changeIndex,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
