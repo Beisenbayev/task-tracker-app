4f18ee248ea7cbed67c7ba567997361bf786f908
beisenbayev <beisenbayeff.nursultan@mail.ru>
2022-01-04 22:54:29 +0600

    edit-category handler was added

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:to_do_app/app/home/widgets/categories_widget.dart';
 import 'package:to_do_app/app/home/widgets/task_list_widget.dart';
 import 'package:to_do_app/core/consts/padding_consts.dart';
+import 'package:to_do_app/core/models/home_page_model.dart';
 import 'package:to_do_app/core/theme/colors_theme.dart';
 import 'package:to_do_app/core/widgets/tap_bar_widget.dart';
+import 'package:to_do_app/router/routes.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -13,16 +15,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _index = 0;
+  int _tabIndex = 0;
+  int _categoryIndex = 0;
 
-  void _changeIndex(int index) {
+  void _changeTabIndex(int index) {
     setState(() {
-      _index = index;
+      _tabIndex = index;
     });
+  }
+
+  void _changeCategoryIndex(int index) {
+    setState(() {
+      _categoryIndex = index;
+    });
+  }
+
+  void _editCategory(int index) {
+    Navigator.of(context).pushNamed(
+      RouteAliasData.category,
+      arguments: index,
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    final _model = HomePageProvider.of(context)!.model;
+
     return Scaffold(
       backgroundColor: ColorThemeShelf.primaryBackground,
       body: SafeArea(
@@ -33,16 +51,21 @@ class _HomePageState extends State<HomePage> {
                 horizontal: PaddingConsts.horizontal,
               ),
               child: TapBarWidget(
-                currentIndex: _index,
+                currentIndex: _tabIndex,
                 items: const <TapBarWidgetItem>[
                   TapBarWidgetItem(text: 'Task List'),
                   TapBarWidgetItem(text: 'Completed')
                 ],
-                onTap: _changeIndex,
+                onTap: _changeTabIndex,
               ),
             ),
             const SizedBox(height: 24),
-            const CategoriesWidget(),
+            CategoriesWidget(
+              categoris: _model.categories,
+              selectedIndex: _categoryIndex,
+              handleChangeCategory: _changeCategoryIndex,
+              handleEditCategory: _editCategory,
+            ),
             const SizedBox(height: 33),
             const Expanded(
               child: TaskListWidget(),
