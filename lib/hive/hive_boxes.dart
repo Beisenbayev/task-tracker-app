1a58f40a97d96636031d5a6e79f1daaf3ef14797
beisenbayev <beisenbayeff.nursultan@mail.ru>
2022-01-04 21:37:39 +0600

    hive: category box

--- lib/hive/hive_boxes.dart
+++ lib/hive/hive_boxes.dart
@@ -1,7 +1,15 @@
 import 'package:hive/hive.dart';
 import 'entity/task.dart';
 
+class HiveBoxAlias {
+  static String categories = 'categories_box';
+  static String archives = 'archives_box';
+  static String tasks = 'tasks_box';
+}
+
 class HiveBoxes {
-  static Box<Task> getTasksBox() => Hive.box<Task>('tasks_box');
-  static Box<Task> getArchivesBox() => Hive.box<Task>('archives_box');
+  static Box<Task> getCategoriesBox() =>
+      Hive.box<Task>(HiveBoxAlias.categories);
+  static Box<Task> getTasksBox() => Hive.box<Task>(HiveBoxAlias.tasks);
+  static Box<Task> getArchivesBox() => Hive.box<Task>(HiveBoxAlias.archives);
 }
