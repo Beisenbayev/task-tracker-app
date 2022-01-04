52fb47e855f0d39e40f6ae29ee3c76631e25dad7
beisenbayev <beisenbayeff.nursultan@mail.ru>
2022-01-04 21:38:00 +0600

    icon data storage

---
+++ lib/core/storage/icon_data_storage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class IconsCollection {
+  static const List<IconData> icons = [
+    Icons.list_sharp,
+    Icons.work_outline_rounded,
+    Icons.cake_outlined,
+    Icons.sports_basketball,
+    Icons.pedal_bike_rounded,
+    Icons.phone_android_outlined,
+    Icons.build_outlined,
+    Icons.menu_book_rounded,
+    Icons.computer_outlined,
+    Icons.mail,
+    Icons.attach_money_rounded,
+    Icons.people_alt,
+  ];
+}
