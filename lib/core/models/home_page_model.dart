8d762d11e45d0b7ffe4e57b5aceba68ebab7d713
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-14 20:56:00 +0600

    home-page-model was updated

--- lib/core/models/home_page_model.dart
+++ lib/core/models/home_page_model.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/cupertino.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:to_do_app/hive/entity/category.dart';
+import 'package:to_do_app/hive/entity/task.dart';
 import 'package:to_do_app/hive/hive_boxes.dart';
 
 class HomePageModel extends ChangeNotifier {
-  final _categoriesBox = HiveBoxes.getCategoriesBox();
+  int _categoryIndex = 0;
+  late int _categoryKey;
+  final Box<Category> _categoriesBox = HiveBoxes.getCategoriesBox();
+  late Box<Task> _tasksBox;
   List<Category> _categories = [];
+  List<Task> _tasks = [];
 
+  int get categoryIndex => _categoryIndex;
   List<Category> get categories => _categories;
+  List<Task> get tasks => _tasks;
 
   HomePageModel() {
     _setup();
   }
 
   void _setup() {
-    _notifyListenChange();
-    _categoriesBox.listenable().addListener(_notifyListenChange);
+    _notifyCategoriesChange();
+    _categoriesBox.listenable().addListener(_notifyCategoriesChange);
   }
 
-  _notifyListenChange() {
+  void _notifyCategoriesChange() {
     _categories = _categoriesBox.values.toList();
+    notifyListeners();
+  }
+
+  void _notifyTasksChange() {
+    _tasks = _tasksBox.values.toList();
+    notifyListeners();
+  }
+
+  void _initTasksByKey() async {
+    _categoryKey = _categoriesBox.keyAt(_categoryIndex);
+    _tasksBox = await HiveBoxes.getTasksBox(_categoryKey);
+    _tasksBox.listenable().addListener(_notifyTasksChange);
+    _tasks = _tasksBox.values.toList();
+  }
+
+  void changeCategoryIndex(int index) {
+    _categoryIndex = index;
+    _initTasksByKey();
     notifyListeners();
   }
 }
