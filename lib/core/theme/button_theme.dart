7ef599620c2696d83b6234f5ddc32f4f0ada4030
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-17 08:30:32 +0600

    disabled-button theme

--- lib/core/theme/button_theme.dart
+++ lib/core/theme/button_theme.dart
@@ -21,4 +21,24 @@ class ButtonThemeShelf {
           ),
         ),
       );
+
+  static ButtonStyle disabledButton(double vertical, double horizontal) =>
+      ButtonStyle(
+        backgroundColor:
+            MaterialStateProperty.all(ColorThemeShelf.unselectedBackground),
+        foregroundColor:
+            MaterialStateProperty.all(ColorThemeShelf.primaryLight),
+        textStyle: MaterialStateProperty.all(TextThemeShelf.button),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+        padding: MaterialStateProperty.all(
+          EdgeInsets.symmetric(
+            vertical: vertical,
+            horizontal: horizontal,
+          ),
+        ),
+      );
 }
