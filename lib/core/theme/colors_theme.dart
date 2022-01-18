b937543fc6642d29d4fbb91dc4163516abecaa24
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-18 16:18:55 +0600

    colors-theme: primary-grey

--- lib/core/theme/colors_theme.dart
+++ lib/core/theme/colors_theme.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 class ColorThemeShelf {
   static const Color primaryDark = Color(0xff242424);
   static const Color primaryLight = Color(0xffffffff);
+  static const Color primaryGrey = Color(0xffc4c4c4);
 
   static const Color subtitleColor = Color(0xff757575);
 
-  static const primaryBackground = Color(0xffffffff);
-  static const secondaryBackground = Color(0xfff2f2f2);
+  static const Color primaryBackground = Color(0xffffffff);
+  static const Color secondaryBackground = Color(0xfff2f2f2);
 
-  static const selectedBackground = Color.fromRGBO(149, 250, 113, 0.37);
-  static const selectedForeground = Color(0xff52d22e);
+  static const Color selectedBackground = Color.fromRGBO(149, 250, 113, 0.37);
+  static const Color selectedForeground = Color(0xff52d22e);
 
-  static const unselectedBackground = Color(0xffd9d9d9);
+  static const Color unselectedBackground = Color(0xffd9d9d9);
 
-  static const disabledBackground = Color(0xffc9c9c9);
+  static const Color disabledBackground = Color(0xffc9c9c9);
 }
