86c294ca82318168fdfabc43cbd9abf8d86adad1
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-19 12:27:41 +0600

    avatar render fix

--- lib/core/widgets/circular_avatar.dart
+++ lib/core/widgets/circular_avatar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_app/core/storage/app_data_storage.dart';
+import 'package:to_do_app/core/storage/avatar_image_storage.dart';
 import 'package:to_do_app/router/routes.dart';
 
-class CircularAvatarWidget extends StatelessWidget {
+class CircularAvatarWidget extends StatefulWidget {
   final double diameter;
   final bool? withGesture;
 
@@ -11,28 +13,48 @@ class CircularAvatarWidget extends StatelessWidget {
     this.withGesture,
   }) : super(key: key);
 
+  @override
+  State<CircularAvatarWidget> createState() => _CircularAvatarWidgetState();
+}
+
+class _CircularAvatarWidgetState extends State<CircularAvatarWidget> {
+  AssetImage _image = AvatarCollections.avatars[0].image;
+
+  @override
+  void initState() {
+    super.initState();
+    _getAvatarImage();
+  }
+
+  void _getAvatarImage() async {
+    final localAvatar = await LocalValueEditor.getProfileData();
+    setState(() {
+      _image = AvatarCollections.avatars[localAvatar].image;
+    });
+  }
+
   void handleOnTap(BuildContext context) {
     Navigator.of(context).pushNamed(RouteAliasData.profile);
   }
 
   @override
   Widget build(BuildContext context) {
-    final child = (withGesture != null)
+    final child = (widget.withGesture != null)
         ? GestureDetector(
             onTap: () => handleOnTap(context),
           )
         : const SizedBox.shrink();
 
     return Container(
-      width: diameter,
-      height: diameter,
-      decoration: const BoxDecoration(
+      width: widget.diameter,
+      height: widget.diameter,
+      decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage('./avatars/bear.jpg'),
+          image: _image,
           fit: BoxFit.cover,
         ),
         color: Colors.amber,
-        borderRadius: BorderRadius.all(
+        borderRadius: const BorderRadius.all(
           Radius.circular(100),
         ),
       ),
