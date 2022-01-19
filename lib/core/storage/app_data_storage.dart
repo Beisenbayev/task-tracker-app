86ab4406b191eb74962b2ed84d882b4ad281e863
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-01-19 09:49:19 +0600

    app-data storage for local values

---
+++ lib/core/storage/app_data_storage.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalValueAliasData {
+  static const String firstTime = 'first_time';
+  static const String username = 'username';
+  static const String profileData = 'profile_data';
+}
+
+class LocalValueEditor {
+  static Future<String> getUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    final username = prefs.getString(LocalValueAliasData.username) ?? '';
+    return username;
+  }
+
+  static setUsername(String text) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(LocalValueAliasData.username, text);
+  }
+
+  static Future<int> getProfileData() async {
+    final prefs = await SharedPreferences.getInstance();
+    final profileData = prefs.getInt(LocalValueAliasData.profileData) ?? 0;
+    return profileData;
+  }
+
+  static setProfileData(int number) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(LocalValueAliasData.profileData, number);
+  }
+}
